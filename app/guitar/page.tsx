dfe03b8b2b14a8a7b6647f52397253cd684264a7
Lynus <limlynus@gmail.com>
2025-08-16 01:53:54 +0800

    add heroicons and gear

--- app/guitar/page.tsx
+++ app/guitar/page.tsx
@@ -3,12 +3,14 @@ import Dashboard from "@/app/ui/guitar/dashboard"
 import Image from "next/image"
 import homura from "@/public/homura2.png"
 import InteractiveSpace from "./interactive"
+import { Cog6ToothIcon } from "@heroicons/react/16/solid"
 
 export default function GuitarPage() {
     return(
         <div className="h-full flex flex-col" >
-            <header className="flex-none text-center text-2xl outline-amber-500 outline p-6 m-2">
-                Lynus' Guitar Tool
+            <header className="relative flex-none text-center text-2xl outline-amber-500 outline px-4 py-2 m-2">
+                <h1> Lynus' Guitar Tool </h1>
+                <Cog6ToothIcon className="absolute right-4 inset-y-0 h-5 w-5 self-center"></Cog6ToothIcon>
             </header>
             <InteractiveSpace></InteractiveSpace>
         </div>
