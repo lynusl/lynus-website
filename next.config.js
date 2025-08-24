3c25276ac6e151701bba860423a60263cac012c3
Lynus <limlynus@gmail.com>
2025-08-24 21:40:33 +0800

    fix: copy config to `next.config.js` to try to get it recognised by gh actions

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type { import('next').NextConfig } */
+
+const nextConfig = {
+  /* config options here */
+  // output: "export",
+  basePath: '/lynus-website',
+  assetPrefix: '/lynus-website',
+};
+
+export default nextConfig;
